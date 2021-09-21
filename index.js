ad855270cef0d19d72ff44f1b509063556f76bfa
drull1000 <pcaetanop@protonmail.com>
2021-09-21 20:17:58 -0300

    Adding cors

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const axios = require("axios");
+const bodyParser = require("body-parser");
+//var cors = require('cors');
+
 const app = express();
 
 app.use(express.json({ limit: "1mb" }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(cors());
 
 const getUniversities = async (req, res) => {
   country = req.body.msg;
@@ -11,8 +26,9 @@ const getUniversities = async (req, res) => {
   res.send(response.data);
 };
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.send("Server go brrr");
+  res.status(200).json({ message: "Enable CORS in Node.js - Clue Mediator" });
 });
 
 app.post("/api", async (req, res) => {
@@ -21,8 +37,8 @@ app.post("/api", async (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log("server go brrr. Port" + PORT);
+  console.log("server go brrr. Port " + PORT);
 });
